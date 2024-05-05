d4de94f505d442c00a982811971f2b38692ae98c
garcifer <garcifer@students.zhaw.ch>
2024-05-05 23:34:11 +0200

    rotating logo wip

--- react-draw/src/Menu.js
+++ react-draw/src/Menu.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Menu.css'; // Import a CSS file to style your buttons
 
 import reactLogo from './media/logo.svg';
 
 export default function Menu() {
-    return (         
-            <div className={"container-column"}>
-                <div className="container-row">
-                    <img className={"App-logo"} src={reactLogo} alt={"logo"} />
-                </div>
-                <div className="container-row">
-                    <div className="menu">
-                        <Link to="/draw">
-                            <button className="menu-button">Draw</button>
-                        </Link>
-                    </div>
+    const [prevX, setPrevX] = useState(null);
+    const [rotation, setRotation] = useState(0);
+
+    const handleDrag = (e) => {
+        if (prevX !== null) {
+            setRotation(rotation + (prevX) - e.clientX);
+        }
+        setPrevX(e.clientX);
+    };
+
+    return (
+        <div className={"container-column"}>
+            <div className="container-row">
+                <img className={"App-logo"} src={reactLogo} alt={"logo"}
+                     style={{transform: `rotate(${rotation}deg)`}}
+                     draggable="true" 
+                     onMouseMove={handleDrag} 
+                     onDragStart={e => e.preventDefault()}/>
+            </div>
+            <div className="container-row">
+                <div className="menu">
+                    <Link to="/draw">
+                        <button className="menu-button">Draw</button>
+                    </Link>
                 </div>
             </div>
+        </div>
     );
 }
